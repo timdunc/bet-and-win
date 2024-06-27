f59224f19e41186452e756fade94c9c48c36ad10
‌ Duncan <tim2015duncan@gmail.com>
2024-06-27 14:33:42 +0200

    bet-and-win-V1.03.06 Login and Ragister responsiveness

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,8 @@
 import NavBar from "../components/NavBar";
 import Box from "@mui/material/Box";
-import Drawer from "@mui/material/Drawer";
 import CssBaseline from "@mui/material/CssBaseline";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import Divider from "@mui/material/Divider";
-import LeftBar from "../components/LeftBar";
 import {
   Avatar,
   Button,
@@ -13,26 +10,12 @@ import {
   Container,
   FormControlLabel,
   Grid,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
   TextField,
   Typography,
 } from "@mui/material";
-import HomeIcon from "@mui/icons-material/Home";
-import LiveTvIcon from "@mui/icons-material/LiveTv";
-import AvTimerIcon from "@mui/icons-material/AvTimer";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 
 const drawerWidth = 240;
-
-const noScrollbarStyle = {
-  overflow: "scroll",
-  msOverflowStyle: "none", // Internet Explorer 10+
-  scrollbarWidth: "none", // Firefox
-};
 import { makeStyles } from "@mui/styles";
 import { Link } from "react-router-dom";
 import { useState } from "react";
@@ -40,7 +23,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
+  main: {
     [theme.breakpoints.down("xs")]: {
       backgroundColor: "red",
     },
@@ -77,14 +60,26 @@ function Copyright(props) {
   );
 }
 
+import { useNavigate } from "react-router-dom";
+import DrawerRight from "../components/DrawerRight";
+import DrawerLeft from "../components/DrawerLeft";
+import BottomNav from "../components/BottomNav";
+
 const API_URL = "https://bet-and-win.onrender.com";
 
 const Login = () => {
   const classes = useStyles();
 
+  const Navigate = useNavigate();
+
+  const linkToRegister = () => {
+    Navigate("/register");
+  };
+
   const [phoneNumber, setPhoneNumber] = useState("");
   const [password, setPassword] = useState("");
   const [loggedInUser, setLoggedInUser] = useState({});
+  const [loggedIn, setLoggedIn] = useState(false);
   const [message, setMessage] = useState("");
 
   const handleLogin = (e) => {
@@ -104,6 +99,7 @@ const Login = () => {
         Cookies.set("userCookie", { phonenumber: response.data.phonenumber });
 
         console.log(response.data);
+        Navigate("/");
         // Login was successful, you can redirect the user or show a success message
       })
       .catch((error) => {
@@ -111,181 +107,126 @@ const Login = () => {
         // Handle login error
       });
   };
-  return (
-    <div>
-      <Box sx={{ display: "flex" }}>
-        <CssBaseline />
-        <AppBar
-          position="fixed"
-          sx={{
-            width: `calc(100% - ${drawerWidth}px)`,
-            ml: `${drawerWidth}px`,
-            zIndex: (theme) => theme.zIndex.drawer + 1,
-          }}
-        >
-          <NavBar />
-        </AppBar>
 
-        <Drawer
-          sx={{
-            width: drawerWidth,
-            flexShrink: 0,
-            "& .MuiDrawer-paper": {
-              width: drawerWidth,
-              boxSizing: "border-box",
-            },
-          }}
-          variant="permanent"
-          anchor="left"
-          className={classes.root}
-        >
-          <Toolbar>
-            <Typography
-              variant="h6"
-              component="div"
-              sx={{ flexGrow: 1 }}
-              style={{ fontSize: "28px", fontWeight: "bold" }}
-            >
-              BETandWIN
-            </Typography>
-            {/* <CustomComponent /> */}
-          </Toolbar>
-          <div style={noScrollbarStyle}>
-            <Divider />
-            <List>
-              {["Home", "Live", "Upcomming"].map((text, index) => (
-                <ListItem key={text} disablePadding>
-                  <ListItemButton>
-                    <ListItemIcon style={{ color: "black" }}>
-                      {index === 0 && <HomeIcon />}
-                      {index === 1 && <LiveTvIcon />}
-                      {index === 2 && <AvTimerIcon />}
-                    </ListItemIcon>
-                    <ListItemText primary={text} />
-                  </ListItemButton>
-                </ListItem>
-              ))}
-            </List>
-            <Divider />
-            <Divider />
-            <LeftBar />
-            <Divider />
-          </div>
-        </Drawer>
-        <Box
-          component="main"
-          sx={{
-            flexGrow: 1,
-            bgcolor: "background.default",
-            p: 3,
-          }}
-          style={{}}
-        >
-          <Toolbar />
-          <Container component="main" maxWidth="xs">
-            <CssBaseline />
-            <Box
-              sx={{
-                marginTop: 8,
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-            >
-              <Avatar sx={{ m: 1 }} style={{ backgroundColor: "#212121" }}>
-                <LockOutlinedIcon />
-              </Avatar>
-              <Typography component="h1" variant="h5">
-                Sign in
-              </Typography>
-              <Box component="form" noValidate sx={{ mt: 1 }}>
-                <TextField
-                  required
-                  fullWidth
-                  id="phoneNumber"
-                  label="Phone Number"
-                  name="phoneNumber"
-                  type="number"
-                  value={phoneNumber}
-                  onChange={(event) => {
-                    setPhoneNumber(event.target.value);
-                  }}
-                />
-                <TextField
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  value={password}
-                  onChange={(event) => {
-                    setPassword(event.target.value);
-                  }}
-                />
-                <FormControlLabel
-                  control={
-                    <Checkbox value="remember" style={{ color: "#212121" }} />
-                  }
-                  label="Remember me"
-                />
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  sx={{ mt: 3, mb: 2 }}
-                  style={{ backgroundColor: "#212121" }}
-                  onClick={handleLogin}
-                >
-                  {/* {isFetching ? (
+  return (
+    <>
+      <div>
+        <Box sx={{ display: "flex" }} className={classes.main}>
+          <CssBaseline />
+          <AppBar
+            position="fixed"
+            sx={{
+              width: `calc(100% - ${drawerWidth}px)`,
+              ml: `${drawerWidth}px`,
+              zIndex: (theme) => theme.zIndex.drawer + 1,
+            }}
+          >
+            <NavBar loggedIn={loggedIn} loggedInUser={loggedInUser} />
+          </AppBar>
+
+          <DrawerLeft />
+          <Box
+            component="main"
+            sx={{
+              flexGrow: 1,
+              bgcolor: "background.default",
+              p: 3,
+            }}
+            style={{}}
+          >
+            <Toolbar />
+            <Container component="main" maxWidth="xs">
+              <CssBaseline />
+              <Box
+                sx={{
+                  marginTop: 8,
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                }}
+              >
+                <Avatar sx={{ m: 1 }} style={{ backgroundColor: "#212121" }}>
+                  <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                  Sign in
+                </Typography>
+                <Box component="form" noValidate sx={{ mt: 1 }}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="phoneNumber"
+                    label="Phone Number"
+                    name="phoneNumber"
+                    type="number"
+                    value={phoneNumber}
+                    onChange={(event) => {
+                      setPhoneNumber(event.target.value);
+                    }}
+                  />
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={(event) => {
+                      setPassword(event.target.value);
+                    }}
+                  />
+                  <FormControlLabel
+                    control={
+                      <Checkbox value="remember" style={{ color: "#212121" }} />
+                    }
+                    label="Remember me"
+                  />
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                    style={{ backgroundColor: "#212121" }}
+                    onClick={handleLogin}
+                  >
+                    {/* {isFetching ? (
                 <CircularProgress color="white" size="20px" />
               ) : ( */}
-                  Sign In
-                  {/* )} */}
-                </Button>
-                <Grid container>
-                  <Grid item xs>
-                    <Link href="#" variant="body2">
-                      Forgot password?
-                    </Link>
+                    Sign In
+                    {/* )} */}
+                  </Button>
+                  <Grid container>
+                    <Grid item xs>
+                      <Link
+                        variant="body2"
+                        style={{ textDecoration: "none", color: "black" }}
+                      >
+                        Forgot password?
+                      </Link>
+                    </Grid>
+                    <Grid item>
+                      <Link
+                        href="#"
+                        variant="body2"
+                        style={{ textDecoration: "none", color: "black" }}
+                        onClick={linkToRegister}
+                      >
+                        {"Don't have an account? Sign Up"}
+                      </Link>
+                    </Grid>
                   </Grid>
-                  <Grid item>
-                    <Link
-                      href="#"
-                      variant="body2"
-                      style={{ textDecoration: "none" }}
-                      // onClick={handleClickOpenSignup}
-                    >
-                      {"Don't have an account? Sign Up"}
-                    </Link>
-                  </Grid>
-                </Grid>
+                </Box>
               </Box>
-            </Box>
-            <Copyright sx={{ mt: 8, mb: 4 }} />
-          </Container>
+              <Copyright sx={{ mt: 8, mb: 4 }} />
+            </Container>
+          </Box>
+          <DrawerRight />
         </Box>
-        <Drawer
-          sx={{
-            width: drawerWidth,
-            flexShrink: 0,
-            "& .MuiDrawer-paper": {
-              width: drawerWidth,
-              boxSizing: "border-box",
-            },
-          }}
-          variant="permanent"
-          anchor="right"
-        >
-          <Toolbar />
-          <Divider />
-          <LeftBar />
-          <Divider />
-          <LeftBar />
-        </Drawer>
-      </Box>
-    </div>
+      </div>
+      <BottomNav />
+    </>
   );
 };
 
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,246 @@
+import NavBar from "../components/NavBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import {
+  Avatar,
+  Button,
+  Checkbox,
+  Container,
+  FormControlLabel,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 
+import Cookies from "js-cookie";
 
-const Register = () => {
+const drawerWidth = 240;
+
+import { makeStyles } from "@mui/styles";
+import axios from "axios";
+import { useState } from "react";
+
+function Copyright(props) {
   return (
-    <div>Register</div>
-  )
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://betandwin.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
 }
 
-export default Register
+const useStyles = makeStyles((theme) => ({
+  main: {
+    [theme.breakpoints.down("xs")]: {
+      backgroundColor: "red",
+    },
+    [theme.breakpoints.up("sm")]: {
+      backgroundColor: "green",
+    },
+    [theme.breakpoints.up("md")]: {
+      backgroundColor: "blue",
+    },
+    [theme.breakpoints.up("lg")]: {
+      backgroundColor: "purple",
+    },
+    [theme.breakpoints.up("xl")]: {
+      backgroundColor: "orange",
+    },
+  },
+}));
+
+import { useNavigate, Link } from "react-router-dom";
+import DrawerRight from "../components/DrawerRight";
+import DrawerLeft from "../components/DrawerLeft";
+import BottomNav from "../components/BottomNav";
+
+const API_URL = "https://bet-and-win.onrender.com";
+
+const Register = () => {
+  const classes = useStyles();
+
+  const Navigate = useNavigate();
+
+  const linkToLogin = () => {
+    Navigate("/login");
+  };
+
+  const linkToHome = () => {
+    Navigate("/");
+  };
+
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [password, setPassword] = useState("");
+  const [loggedInUser, setLoggedInUser] = useState({});
+  const [registered, setRegistered] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    axios
+      .post(`${API_URL}/api/auth/register`, {
+        phonenumber: phoneNumber,
+        pin: password,
+      })
+      .then((response) => {
+        setLoggedInUser(response.data);
+
+        localStorage.setItem(
+          "user",
+          JSON.stringify({ phonenumber: response.data.phonenumber })
+        );
+        Cookies.set("userCookie", { phonenumber: response.data.phonenumber });
+
+        setMessage(response.data.message);
+
+        setRegistered(true);
+
+        // setState({
+        //   open: true,
+        //   Transition: Fade,
+        // });
+
+        console.log(response.data);
+        Navigate("/login");
+        // Registration was successful, you can redirect the user or show a success message
+      })
+      .catch((error) => {
+        console.log(error);
+        // Handle register error
+      });
+  };
+
+  return (
+    <>
+      <div>
+        <Box sx={{ display: "flex" }} className={classes.main}>
+          <CssBaseline />
+          <AppBar
+            position="fixed"
+            sx={{
+              width: `calc(100% - ${drawerWidth}px)`,
+              ml: `${drawerWidth}px`,
+              zIndex: (theme) => theme.zIndex.drawer + 1,
+            }}
+          >
+            <NavBar />
+          </AppBar>
+
+          <DrawerLeft />
+          <Box
+            component="main"
+            sx={{
+              flexGrow: 1,
+              bgcolor: "background.default",
+              p: 3,
+            }}
+            style={{}}
+          >
+            <Toolbar />
+            <Container component="main" maxWidth="xs">
+              <CssBaseline />
+              <Box
+                sx={{
+                  marginTop: 8,
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                }}
+              >
+                <Avatar sx={{ m: 1 }} style={{ backgroundColor: "#212121" }}>
+                  <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                  Sign up
+                </Typography>
+                <Box component="form" noValidate sx={{ mt: 3 }}>
+                  <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                      <TextField
+                        required
+                        fullWidth
+                        id="phoneNumber"
+                        label="Phone Number"
+                        name="phoneNumber"
+                        autoComplete="phoneNumber"
+                        type="number"
+                        value={phoneNumber}
+                        onChange={(event) => {
+                          setPhoneNumber(event.target.value);
+                        }}
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={(event) => {
+                          setPassword(event.target.value);
+                        }}
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <FormControlLabel
+                        control={
+                          <Checkbox
+                            value="allowExtraEmails"
+                            style={{ color: "#212121" }}
+                          />
+                        }
+                        label="I want to receive inspiration, marketing promotions and updates via email."
+                      />
+                    </Grid>
+                  </Grid>
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                    style={{ backgroundColor: "#212121" }}
+                    onClick={handleRegister}
+                  >
+                    Sign Up
+                  </Button>
+                  <Grid container justifyContent="flex-end">
+                    <Grid item>
+                      <Link
+                        href="#"
+                        variant="body2"
+                        onClick={linkToLogin}
+                        style={{ textDecoration: "none", color: "black" }}
+                      >
+                        Already have an account? Sign in
+                      </Link>
+                    </Grid>
+                  </Grid>
+                </Box>
+              </Box>
+              <Copyright sx={{ mt: 5 }} />
+            </Container>
+          </Box>
+          <DrawerRight />
+        </Box>
+      </div>
+      <BottomNav />
+    </>
+  );
+};
+
+export default Register;
